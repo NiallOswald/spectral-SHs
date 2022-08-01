e02a299ada71421742af8bfd497400a5be78f7cb
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-08-01 11:49:58 +0100

    Update package name to spectral_shs

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages  # noqa: D100
 
-setup(name="spectral_SHs", version="0.1", packages=find_packages())
+setup(name="spectral_shs", version="0.1", packages=find_packages())
