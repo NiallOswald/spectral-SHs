d064a967b04354088581eaad55d732cea55a9d18
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-08-12 11:47:00 +0100

    First implementation of shs_long_both.py

---
+++ scripts/shs_long_both.py
@@ -0,0 +1,99 @@
+"""Find the flow of a channel with longitudinal grooves on both walls."""
+
+import numpy as np
+import scipy.interpolate as sp
+from spectral_shs import cheb
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
+
+# Setting parameters
+H = 1
+E = 2
+e = 1
+
+L = E / H
+l = e / H  # noqa: E741
+
+# Construct the differentiation matrices
+n = 10
+D, c = cheb(n)
+Dx = np.kron(np.eye(n + 1), D)
+Dy = np.kron(D, np.eye(n + 1))
+
+# Construct the grid of points
+x = np.concatenate((l / 4 * (c - 1), ((L - l) * c - l - L) / 4))
+y = (c + 1) / 2
+xx, yy = np.meshgrid(x, y)
+xf = xx.flatten()
+yf = yy.flatten()
+
+# Construct the operators for each domain
+D2 = D @ D
+D2x = np.kron(np.eye(n + 1), D2)
+D2y = np.kron(D2, np.eye(n + 1))
+L1 = 16 / l**2 * D2x + 4 * D2y
+L2 = 16 / (L - l) ** 2 * D2x + 4 * D2y
+f = -np.ones(2 * (n + 1) ** 2)
+
+# First domain boundary conditions
+L1[: n + 1, :] = Dy[: n + 1, :]  # Interface eta = 1
+f[: n + 1] = 0
+
+L1[(n + 1) * n :, :] = Dy[(n + 1) * n :, :]  # Symmetry eta = -1
+f[: n + 1 : (n + 1) ** 2] = 0
+
+L1[: (n + 1) ** 2 : n + 1, :] = Dx[
+    : (n + 1) ** 2 : n + 1, :
+]  # Symmetry xi = 1
+f[(n + 1) * n : (n + 1) ** 2] = 0
+
+# Second domain boundary conditions
+L2[: n + 1, :] = np.eye(n + 1, (n + 1) ** 2)  # No-slip eta = 1
+f[(n + 1) ** 2 : (n + 1) * (n + 2)] = 0
+
+L2[(n + 1) * n :, :] = Dy[(n + 1) * n :, :]  # Symmetry eta = -1
+f[(n + 1) * (2 * n + 1) * n :] = 0
+
+L2[n : (n + 1) ** 2 : n + 1, :] = Dx[
+    n : (n + 1) ** 2 : n + 1, :
+]  # Symmetry xi = -1
+f[(n + 1) ** 2 + n :: n + 1] = 0
+
+# Add constraints to link the domains
+L1[n : (n + 1) ** 2 : n + 1, :] = Dx[n : (n + 1) ** 2 : n + 1, :]
+A1 = np.zeros(((n + 1) ** 2, (n + 1) ** 2))
+A1[n : (n + 1) ** 2 : n + 1, :] = -Dx[: (n + 1) ** 2 : n + 1, :]
+f[n : (n + 1) ** 2 : n + 1] = 0
+
+L2[: (n + 1) ** 2 : n + 1, :] = np.eye(n + 1, (n + 1) ** 2)
+A2 = np.zeros(((n + 1) ** 2, (n + 1) ** 2))
+A2[: (n + 1) ** 2 : n + 1, n : (n + 1) ** 2 : n + 1] = -np.eye(n + 1, n + 1)
+f[(n + 1) ** 2 : 2 * (n + 1) ** 2 : n + 1] = 0
+
+L = np.hstack((np.vstack((L1, A2)), np.vstack((A1, L2))))
+
+print(L[(n + 1) ** 2 :: n + 1, :])
+
+# Solve the linear system
+u = np.linalg.solve(L, f)
+uu = u.reshape((n + 1, 2 * (n + 1)))
+
+# Create a finer grid for plotting
+x_fine = np.linspace(-L / (2 * H), 0, 41)
+y_fine = np.linspace(0, 1, 21)
+xxx, yyy = np.meshgrid(x_fine, y_fine)
+uuu = sp.griddata(
+    (xx.ravel(), yy.ravel()), uu.ravel(), (xxx, yyy), method="cubic"
+)
+
+# Plot the solution
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(projection="3d")
+ax.plot_surface(xx, yy, uu, rstride=1, cstride=1, cmap="viridis")
+ax.set_xlabel("$x$")
+ax.set_ylabel("$y$")
+ax.set_zlabel("$u$")
+# ax.set_xlim(-L / 2, 0)
+# ax.set_ylim(0, 1)
+
+plt.show()
