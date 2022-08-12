1b9921c1faf609a43c42d5c1db3ff52c214d5936
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-08-12 15:59:05 +0100

    Fix boundary conditions

--- scripts/shs_long_both.py
+++ scripts/shs_long_both.py
@@ -23,7 +23,11 @@ Dy = np.kron(D, np.eye(n + 1))
 # Construct the grid of points
 x = np.concatenate((l / 4 * (c - 1), ((L - l) * c - l - L) / 4))
 y = (c + 1) / 2
-xx, yy = np.meshgrid(x, y)
+xx_std, yy_std = np.meshgrid(x, y)
+
+xx = np.vstack((xx_std[:, : n + 1], xx_std[:, n + 1 :]))
+yy = np.vstack((yy_std[:, : n + 1], yy_std[:, n + 1 :]))
+
 xf = xx.flatten()
 yf = yy.flatten()
 
@@ -36,27 +40,27 @@ L2 = 16 / (L - l) ** 2 * D2x + 4 * D2y
 f = -np.ones(2 * (n + 1) ** 2)
 
 # First domain boundary conditions
-L1[: n + 1, :] = Dy[: n + 1, :]  # Interface eta = 1
+L1[: n + 1, :] = Dy[: n + 1, :]  # Interface y = 1, eta = 1
 f[: n + 1] = 0
 
-L1[(n + 1) * n :, :] = Dy[(n + 1) * n :, :]  # Symmetry eta = -1
-f[: n + 1 : (n + 1) ** 2] = 0
+L1[(n + 1) * n :, :] = Dy[(n + 1) * n :, :]  # Symmetry y = 0, eta = -1
+f[(n + 1) * n : (n + 1) ** 2] = 0
 
 L1[: (n + 1) ** 2 : n + 1, :] = Dx[
     : (n + 1) ** 2 : n + 1, :
-]  # Symmetry xi = 1
-f[(n + 1) * n : (n + 1) ** 2] = 0
+]  # Symmetry x = 0, xi = 1
+f[: (n + 1) ** 2 : n + 1] = 0
 
 # Second domain boundary conditions
-L2[: n + 1, :] = np.eye(n + 1, (n + 1) ** 2)  # No-slip eta = 1
+L2[: n + 1, :] = np.eye(n + 1, (n + 1) ** 2)  # No-slip y = 1, eta = 1
 f[(n + 1) ** 2 : (n + 1) * (n + 2)] = 0
 
-L2[(n + 1) * n :, :] = Dy[(n + 1) * n :, :]  # Symmetry eta = -1
-f[(n + 1) * (2 * n + 1) * n :] = 0
+L2[(n + 1) * n :, :] = Dy[(n + 1) * n :, :]  # Symmetry y = 0, eta = -1
+f[(n + 1) * (2 * n + 1) :] = 0
 
 L2[n : (n + 1) ** 2 : n + 1, :] = Dx[
     n : (n + 1) ** 2 : n + 1, :
-]  # Symmetry xi = -1
+]  # Symmetry x = -L/2, xi = -1
 f[(n + 1) ** 2 + n :: n + 1] = 0
 
 # Add constraints to link the domains
@@ -65,31 +69,29 @@ A1 = np.zeros(((n + 1) ** 2, (n + 1) ** 2))
 A1[n : (n + 1) ** 2 : n + 1, :] = -Dx[: (n + 1) ** 2 : n + 1, :]
 f[n : (n + 1) ** 2 : n + 1] = 0
 
-L2[: (n + 1) ** 2 : n + 1, :] = np.eye(n + 1, (n + 1) ** 2)
+L2[: (n + 1) ** 2 : n + 1, :] = np.zeros((n + 1, (n + 1) ** 2))
+L2[: (n + 1) ** 2 : n + 1, : (n + 1) ** 2 : n + 1] = np.eye(n + 1, n + 1)
 A2 = np.zeros(((n + 1) ** 2, (n + 1) ** 2))
 A2[: (n + 1) ** 2 : n + 1, n : (n + 1) ** 2 : n + 1] = -np.eye(n + 1, n + 1)
 f[(n + 1) ** 2 : 2 * (n + 1) ** 2 : n + 1] = 0
 
-L = np.hstack((np.vstack((L1, A2)), np.vstack((A1, L2))))
-
-print(L[(n + 1) ** 2 :: n + 1, :])
+L = np.block([[L1, A1], [A2, L2]])
 
 # Solve the linear system
 u = np.linalg.solve(L, f)
-uu = u.reshape((n + 1, 2 * (n + 1)))
+uu = u.reshape((2 * (n + 1), n + 1))
+uu_std = np.hstack((uu[: n + 1, :], uu[n + 1 :, :]))
 
 # Create a finer grid for plotting
-x_fine = np.linspace(-L / (2 * H), 0, 41)
+x_fine = np.linspace(-L / 2, 0, 41)
 y_fine = np.linspace(0, 1, 21)
 xxx, yyy = np.meshgrid(x_fine, y_fine)
-uuu = sp.griddata(
-    (xx.ravel(), yy.ravel()), uu.ravel(), (xxx, yyy), method="cubic"
-)
+# uuu = sp.griddata((xf, yf), u, (xxx, yyy), method="cubic")
 
 # Plot the solution
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(projection="3d")
-ax.plot_surface(xx, yy, uu, rstride=1, cstride=1, cmap="viridis")
+ax.plot_surface(xx_std, yy_std, uu_std, rstride=1, cstride=1, cmap="viridis")
 ax.set_xlabel("$x$")
 ax.set_ylabel("$y$")
 ax.set_zlabel("$u$")
